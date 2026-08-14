73ff7a4f7634bbfb5fb87a9eed0620d0c93fbbd4
Akansha Jain <akanshajn2005@gmail.com>
2026-08-14 21:59:51 +0530

    Time: 0 ms (100%), Space: 10.7 MB (57.38%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -2,17 +2,6 @@ class Solution {
 public:
     vector<int> countBits(int n) {
         vector<int>ans(n+1, 0);
-        // for(int i=0; i<=n; i++)
-        // {
-        //     int j=i;
-        //     int cnt =0;
-        //     while(j>0)
-        //     {
-        //         if(j%2 !=0)cnt++;
-        //         j/=2;
-        //     }
-        //     ans[i] = cnt;
-        // }
         for(int i = 1; i <= n; i++)
             ans[i] = ans[i >> 1] + (i & 1);
         return ans;
