7f8cebcb65b4d3072a370ec9ba563b5bec1d5d9e
Akansha Jain <akanshajn2005@gmail.com>
2026-09-24 23:00:39 +0530

    Time: 0 ms (100%), Space: 15.3 MB (37.63%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -4,23 +4,18 @@ public:
         int l=0, r=nums.size()-1;
         while(l<=r)
         {
-            int mid = l +  (r-l)/2;
-            if(nums[mid]==target) return mid;
-            if(nums[l]<=nums[mid])
+            int mid = l + (r-l)/2;
+            if(target == nums[mid]) return mid;
+            if(nums[l] <= nums[mid])
             {
-                if(target>=nums[l] && nums[mid]>=target)
-                r = mid-1;
-                else
-                {
-                    l = mid+1;
-                }
+                if(target >= nums[l] && target <=nums[mid]) r = mid-1;
+                else l = mid+1;
             }
-            else 
+            else // when (nums[l] >nums[mid])
             {
-                if(target<=nums[r] && target>=nums[mid]) l = mid+1;
-                else
-                  r = mid-1;
-            }   
+                if(target <= nums[r] && target>=nums[mid]) l = mid+1;
+                else r = mid-1;
+            }
         }
         return -1;
     }
